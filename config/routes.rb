410ad0ce7121ac5262028e0a57ddf0d30c079f0e
sannaga3 <n.tetsuya02@gmail.com>
2021-06-04 18:30:45 +0900

    追加: ユーザーモデルにカラム追加 #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   resources :foods
 end
