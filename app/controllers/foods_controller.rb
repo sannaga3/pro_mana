b987f7b498de32b612deb9d6644776a378c8f566
sannaga3 <n.tetsuya02@gmail.com>
2021-06-04 17:06:15 +0900

    追加: 食品登録機能作成 #6

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,15 @@
 class FoodsController < ApplicationController
+  
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+      if @food.save
+        redirect_to foods_path, notice: "食品登録完了"
+      else
+        render :new
+      end
+  end
 end
