41757f295243085cd26ae4aae3b47b1c439079a9
sannaga3 <n.tetsuya02@gmail.com>
2021-06-05 10:52:29 +0900

    追加： 食品記録機能作成 #16

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,9 +3,17 @@ class RecordsController < ApplicationController
   end
 
   def new
+    @record = Record.new
+    @foods = Food.where(user_id: current_user.id)
   end
 
   def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to records_path, notice: "食品登録完了"
+    else
+      render :new
+    end
   end
 
   def show
