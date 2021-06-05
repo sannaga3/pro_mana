6b8c0d0e67173eea5995b90d393e327cbf07a740
sannaga3 <n.tetsuya02@gmail.com>
2021-06-05 22:33:26 +0900

    追加: friendshipsコントローラ作成 #18

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'friendships/create'
+  get 'friendships/destroy'
   devise_for :users, :controllers => {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
