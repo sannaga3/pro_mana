e6be8e2bc268fec0213fff26517dc52d5f9a1ace
sannaga3 <n.tetsuya02@gmail.com>
2021-06-05 22:43:21 +0900

    追加: friendshipコントローラのcreateアクション作成 #18

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,11 @@
 class FriendshipsController < ApplicationController
-  def create
+  class RelationshipsController < ApplicationController
+    before_action :authenticate_user!
+    respond_to? :js
+    def create
+      @user = User.find(params[:friendship][:followed_id])
+      current_user.follow!(@user)
+    end
   end
 
   def destroy
