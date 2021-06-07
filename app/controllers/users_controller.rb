801cd7e0177ef9b51a49c2cbb0d8dd24c391c3cc
sannaga3 <n.tetsuya02@gmail.com>
2021-06-07 13:56:37 +0900

    修正: deviseの国際化時にサインアップのnameカラムが削除されていたのを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    friendships = Friendship.where(follower_id: current_user.id).pluck(:followed_id)
+    @followed_users = User.find(friendships)
+    # binding.irb
+  end
+
   def show
     @user = User.find(params[:id])
   end
