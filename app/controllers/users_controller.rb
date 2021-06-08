3799d100b5822b8be7a23f4ced22f9db4440666e
sannaga3 <n.tetsuya02@gmail.com>
2021-06-08 21:12:00 +0900

    追加: プロフィール編集ページの簡易css設定 #37

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def index
     friendships = Friendship.where(follower_id: current_user.id).pluck(:followed_id)
     @followed_users = User.find(friendships)
-    # binding.irb
   end
 
   def show
