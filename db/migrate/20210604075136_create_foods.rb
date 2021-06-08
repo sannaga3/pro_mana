d99b83b2cffd1d7ab56ed14b73ef847b4fa1b9dd
sannaga3 <n.tetsuya02@gmail.com>
2021-06-09 06:14:00 +0900

    foodsテーブルのprotainがスペル間違いの為、proteinに修正 #39

--- db/migrate/20210604075136_create_foods.rb
+++ db/migrate/20210604075136_create_foods.rb
@@ -2,7 +2,7 @@ class CreateFoods < ActiveRecord::Migration[5.2]
   def change
     create_table :foods do |t|
       t.string :name, null: false
-      t.integer :protain, null: false
+      t.integer :protein, null: false
       t.integer :quantity, null: false
       t.string :unit, null: false
       t.timestamps
