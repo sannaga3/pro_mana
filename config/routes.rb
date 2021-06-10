43813df0851d0de472cdce3f77b151cb11b60028
sannaga3 <n.tetsuya02@gmail.com>
2021-06-10 22:09:27 +0900

    追加: デプロイ時エラーの為、Herokuでbundlerが実行できるようにプラットフォーム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get 'friendships/create'
-  get 'friendships/destroy'
   devise_for :users, :controllers => {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,41 @@
-# 40.times do |i|
-#   User.create!(
-#     email: "#{i}@example.com",
-#     name: "ユーザー#{i}",
-#     password: "aaaaaa",
-#     admin: false
-#   )
-# end
+User.create!(
+  email: "a@example.com",
+  name: "ユーザーa",
+  password: "aaaaaa",
+  admin: true
+)
 
-# 20.times do |n|
-#   Food.create!(
-#     name: "豆腐#{n}",
-#     protein: rand(1..10) ,
-#     quantity: 1,
-#     unit: "個",
-#     user_id: User.first.id
-#   )
-# end
+40.times do |i|
+  User.create!(
+    email: "#{i}@example.com",
+    name: "ユーザー#{i}",
+    password: "aaaaaa",
+    admin: false
+  )
+end
 
-# 5.times do |m|
-#   Record.create!(
-#     ate: 1,
-#     record_on: "2021-06-09",
-#     food_id: Record.first.id + m,
-#     user_id: User.first.id + m
-#   )
-# end
+20.times do |n|
+  Food.create!(
+    name: "豆腐#{n}",
+    protein: rand(1..10) ,
+    quantity: 1,
+    unit: "個",
+    user_id: User.first.id
+  )
+end
 
-# 40.times do |l|
-#   Friendship.create!(
-#     follower_id: User.first.id,
-#     followed_id: User.second.id + 2 + l,
-#   )
-# end
+5.times do |m|
+  Record.create!(
+    ate: 1,
+    record_on: "2021-06-09",
+    food_id: Record.first.id + m,
+    user_id: User.first.id + m
+  )
+end
+
+40.times do |l|
+  Friendship.create!(
+    follower_id: User.first.id,
+    followed_id: User.second.id + 2 + l,
+  )
+end
