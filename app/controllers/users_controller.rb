68bc560f765d4fa0405b18a64412e53adf0604a7
sannaga3 <n.tetsuya02@gmail.com>
2021-06-10 11:21:10 +0900

    修正: 毎日の記録ページのフッターの幅が狭い不具合修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     friendships = Friendship.where(follower_id: current_user.id).pluck(:followed_id)
-    @followed_users = User.find(friendships)
+    @followed_users = User.find(friendships).per(15)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 40.times do |i|
+#   User.create!(
+#     email: "#{i}@example.com",
+#     name: "ユーザー#{i}",
+#     password: "aaaaaa",
+#     admin: false
+#   )
+# end
+
+40.times do |i|
+  Food.create!(
+    name: "卵豆腐#{i}",
+    protein: rand(1..10) ,
+    quantity: "1",
+    unit: "個",
+    user_id: i
+  )
+end
+
+40.times do |i|
+  record.create!(
+    ate: 1,
+    record_on: "Wed, 09 Jun 2021" ,
+    food_id: i,
+    user_id: i + 1
+  )
+end
+
+40.times do |i|
+  Friendship.create!(
+    follower_id: 1,
+    followed_id: i + 1,
+  )
+end
