815a287d197e8e0d7dea1d52bf3e017867ee4dd1
sannaga3 <n.tetsuya02@gmail.com>
2021-06-14 07:32:52 +0900

    追加: RSpecの食品データ作成 #52

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,5 +1,24 @@
 FactoryBot.define do
   factory :food do
-    
+    name { '納豆' }
+    protein { '4' }
+    quantity { '1' }
+    unit { 'パック' }
+    association :user
+  end
+
+  factory :second_food, class: Food do
+    name { '卵' }
+    protein { '4' }
+    quantity { '1' }
+    unit { '個' }
+    association :user
+  end
+
+  factory :third_food, class: Food do
+    name { '豆腐' }
+    protein { '6' }
+    quantity { '100' }
+    association :user
   end
 end
