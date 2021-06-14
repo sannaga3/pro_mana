4fde1d5e1db885855d4ba5195fb3161fed24895e
sannaga3 <n.tetsuya02@gmail.com>
2021-06-15 01:17:21 +0900

    修正: factoriesデータ修正によりrecordのmodelテストが通らなくなっていた為修正 #54

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Record, type: :model do
   let!(:user_one) { FactoryBot.create(:user_one)}
   let!(:food_one) { FactoryBot.create(:food_one) }
-  let(:record) { FactoryBot.create(:record)}
+  let(:record_one) { FactoryBot.create(:record_one)}
   describe '食品記録作成時のバリデーションテスト' do
     context '食事量と日付が正しい場合' do
       it '記録を作成できる' do
-        expect(record).to be_valid
+        expect(record_one).to be_valid
       end
     end
     context '食事量が未入力の場合' do
       it 'バリデーションエラーになる' do
-        record.ate = nil
-        expect(record).to be_invalid
+        record_one.ate = nil
+        expect(record_one).to be_invalid
       end
     end
   end
