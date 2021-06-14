2bf1d6167d787ee581c2c14648a3aa4c76418266
sannaga3 <n.tetsuya02@gmail.com>
2021-06-14 10:31:39 +0900

    修正: 食品の単位に文字数制限が掛かっていなかった為修正 #52

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:food) { FactoryBot.create(:food)}
+  describe '食品新規作成時のバリデーションテスト' do
+    context '名前、タンパク質量、量、単位、が正しい場合' do
+      it '食品を作成できる' do
+        expect(food).to be_valid
+        puts food.user_id
+        puts food.user.name
+      end
+    end
+    context '食品名が未入力の場合' do
+      it 'バリデーションエラーになる' do
+        food.name = nil
+        expect(food).to be_invalid
+      end
+    end
+    context '食品名が51文字以上の場合' do
+      it 'バリデーションエラーになる' do
+        food.name = "納豆"*26
+        expect(food).to be_invalid
+      end
+    end
+    context 'タンパク質量が未入力の場合' do
+      it 'バリデーションエラーになる' do
+        food.protein = ""
+        expect(food).to be_invalid
+      end
+    end
+    context '量が未入力の場合' do
+      it 'バリデーションエラーになる' do
+        food.quantity = ""
+        expect(food).to be_invalid
+      end
+    end
+    context '単位が未入力の場合' do
+      it 'バリデーションエラーになる' do
+        food.unit = ""
+        expect(food).to be_invalid
+      end
+    end
+  end
 end
