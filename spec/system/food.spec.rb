35faa04a592351884e1b9c06db4ba842687ee3bc
sannaga3 <n.tetsuya02@gmail.com>
2021-06-14 11:02:46 +0900

    追加: 食品新規登録時のsystemテスト作成 #52

--- spec/system/food.spec.rb
+++ spec/system/food.spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 describe '食品機能', type: :system do
   describe '食品新規登録テスト' do
+    let!(:user) { FactoryBot.create(:user)}
     let(:food) { FactoryBot.create(:food)}
     before do
       visit new_user_session_path
@@ -12,8 +13,8 @@ describe '食品機能', type: :system do
       expect(page).to have_content 'すだまさき'
       click_on '食品登録'
     end
-    context 'プロフィール画像以外の項目を入力した場合' do
-      it '自身のプロフィールを更新できる(プロフィール画像以外)' do
+    context '食品を新規作成した場合' do
+      it '食品一覧画面の一番最初に表示される' do
         fill_in 'food[name]', with: '鳥もも肉'
         fill_in 'food[protein]', with: '12'
         fill_in 'food[quantity]', with: '50'
@@ -22,7 +23,9 @@ describe '食品機能', type: :system do
         expect(current_path).to eq foods_path
         expect(page).to have_content '食品登録完了'
         expect(page).to have_content '食品一覧'
-        expect(page).to have_content ''
+        expect(page).to have_content '鳥もも肉'
+        food_list = all('.text-center')
+        expect(food_list[0]).to have_content "鳥もも肉"
       end
     end
   end
