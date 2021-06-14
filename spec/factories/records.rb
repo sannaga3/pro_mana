9623e754b3a7f4e8b40743290f0efc2cbf72fe4c
sannaga3 <n.tetsuya02@gmail.com>
2021-06-14 17:18:42 +0900

    追加: RSpecの記録データ作成 #54

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -2,7 +2,28 @@ FactoryBot.define do
   factory :record do
     ate { 1 }
     record_on { "2021-06-05" }
-    food { nil }
-    user { nil }
+    association :user
+    association :food
+  end
+
+  factory :secind_record, class Record do
+    ate { 2 }
+    record_on { "2021-06-06" }
+    association :user
+    association :second_food
+  end
+
+  factory :third_record, class Record do
+    ate { 1 }
+    record_on { "2021-06-07" }
+    association :second_user
+    association :food
+  end
+
+  factory :fourth_record, class Record do
+    ate { 2 }
+    record_on { "2021-06-08" }
+    association :second_user
+    association :second_food
   end
 end
