4e50a2e548b280a3e48589ce55fad7f8590a54ec
sannaga3 <n.tetsuya02@gmail.com>
2021-06-17 13:16:19 +0900

    追加: 食品の登録及び編集でタンパク質量及び基準量にて、マイナスの値を入力及び保存ができないように設定 #61

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food < ApplicationRecord
   validates :name, presence: true, length: { in: 1..50 }
-  validates :protein, presence: true
-  validates :quantity, presence: true
+  validates :protein, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :unit, presence: true, length: { in: 1..10 }
   belongs_to :user
   has_many :records
