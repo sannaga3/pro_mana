e1ba118bc8e347eea3b2f39b3f9a1d457b95266a
sannaga3 <n.tetsuya02@gmail.com>
2021-06-22 07:47:39 +0900

    改善: usersコントローラーをスコープを用いてリファクタリング #82

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ApplicationRecord
   belongs_to :followed, class_name: "User"
   belongs_to :follower, class_name: "User"
+
+  scope :find_current_user, -> (current_user_id) {
+    where(follower_id: current_user_id)
+  }
 end
