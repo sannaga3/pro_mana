ef4aaaff2d8153a53670090e1f6541209bb4a933
sannaga3 <n.tetsuya02@gmail.com>
2021-07-05 19:11:06 +0900

    改善: RecordモデルをNutritionRecordモデルに変更 #118

--- app/models/nutrition_record.rb
+++ app/models/nutrition_record.rb
@@ -1,4 +1,4 @@
-class Record < ApplicationRecord
+class NutritionRecord < ApplicationRecord
   belongs_to :food
   belongs_to :user
   validates :ate, presence: { message: 'を入力してください。' }, numericality: { only_integer: true, greater_than: 0 }
