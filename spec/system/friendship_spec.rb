ff2682386463d39ad862c5a4e2b3c47de7a7223e
sannaga3 <n.tetsuya02@gmail.com>
2021-07-12 13:02:51 +0900

    追加: フォロー一覧ページからのフォロー・フォロー解除テスト追加及びフォロー機能systemスペックテストのリファクタリング #128

--- spec/system/friendship_spec.rb
+++ spec/system/friendship_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Friendship, type: :system do
   let!(:user) { FactoryBot.create(:user) }
   let!(:third_user) { FactoryBot.create(:third_user) }
-  let!(:third_user) { FactoryBot.create(:third_user) }
   let!(:fourth_food) { FactoryBot.create(:fourth_food, user: third_user) }
   let!(:fifth_record) { FactoryBot.create(:fifth_record, user: third_user) }
   describe 'フォロー機能' do
@@ -28,15 +27,12 @@ RSpec.describe Friendship, type: :system do
         expect(table_user_names.count).to eq 1
         find('.table_user_name').click
         expect(current_path).to eq user_path(third_user.id)
-        expect(page).to have_content 'プロフィール'
-        follow_button = all('#follow')
-        expect(follow_button.count).to eq 1
-        find('#follow').click
-        expect(current_path).to eq user_path(third_user.id)
-        unfollow_button = all('#unfollow')
-        expect(1).to eq unfollow_button.count
-        follow_button = all('#follow')
-        expect(0).to eq follow_button.count
+        sleep(0.5)
+        has_selector?('#follow0')
+        find("#follow0").click
+        click_button "フォロー"
+        has_no_selector?('#follow0')
+        has_selector?('#unfollow0')
       end
     end
     context '別ユーザーのプロフィールページでフォロー解除ボタンを押した場合,' do
@@ -48,15 +44,31 @@ RSpec.describe Friendship, type: :system do
         visit user_path(third_user.id)
         expect(current_path).to eq user_path(third_user.id)
         expect(page).to have_content 'プロフィール'
-        unfollow_button = all('#unfollow')
-        expect(unfollow_button.count).to eq 1
-        find('#unfollow').click
+        has_selector?('#unfollow0')
+        find('#unfollow0').click
         sleep(0.5)
-        expect(current_path).to eq user_path(third_user.id)
-        unfollow_button = all('#unfollow')
-        expect(unfollow_button.count).to eq 0
-        follow_button = all('#follow')
-        expect(follow_button.count).to eq 1
+        has_no_selector?('#unfollow0')
+        has_selector?('#follow0')
+      end
+    end
+    context 'フォロー一覧ページから解除ボタンを押し、その後フォローボタンを押した場合,' do
+      it 'フォローが解除され、その後再度フォローされる' do
+        Friendship.create(
+          follower_id: user.id,
+          followed_id: third_user.id
+        )
+        visit users_path
+        expect(current_path).to eq users_path
+        expect(page).to have_content 'フォロー一覧'
+        has_selector?('#unfollow0')
+        find("#unfollow0").click
+        sleep(0.5)
+        has_no_selector?('#unfollow0')
+        has_selector?('#follow0')
+        find("#follow0").click
+        sleep(0.5)
+        has_no_selector?('#follow0')
+        has_selector?('#unfollow0')
       end
     end
   end
