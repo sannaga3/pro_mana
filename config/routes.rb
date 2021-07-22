6ecd08269e62cb9bdda2fde26266d1a08e064062
sannaga3 <n.tetsuya02@gmail.com>
2021-07-22 19:22:22 +0900

    修正: 他人のプロフィールに飛んだ時フォロー以外のボタンが表示されないように修正 #144

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
-    get '/users/sign_out', to: 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'users/sessions#destroy'
   end
   resources :users, only: %i[index show]
   resources :bmis, except: :show
