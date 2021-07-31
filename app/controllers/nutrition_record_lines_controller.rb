8aa15e1fde2ef9c532316f231cccf38091ed0c3e
sannaga3 <n.tetsuya02@gmail.com>
2021-07-31 09:45:20 +0900

    修正: 食事記録削除時に日付記録の持つ食事記録が0個になった場合、親モデルも同時に削除するように修正 #142

--- app/controllers/nutrition_record_lines_controller.rb
+++ app/controllers/nutrition_record_lines_controller.rb
@@ -1,7 +1,9 @@
 class NutritionRecordLinesController < ApplicationController
   def destroy
     @nutrition_record_line = NutritionRecordLine.find(params[:id])
+    @nutrition_record = NutritionRecord.find(@nutrition_record_line.nutrition_record_id)
     @nutrition_record_line.destroy
+    @nutrition_record.destroy if @nutrition_record.nutrition_record_lines.count == 0
     redirect_to request.referer, notice: t('notice.destroy_record')
   end
 end
